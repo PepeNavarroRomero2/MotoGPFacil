652a9135f5bfaae51a62cffc6afaee8e81ebd45e
JoseNavarroRomero <jnavrom0109@g.educaand.es>
2025-02-08 15:33:59 +0100

    o

--- motogpfacil/src/app/pages/pilotos/page.js
+++ motogpfacil/src/app/pages/pilotos/page.js
@@ -1,110 +1,136 @@
 "use client";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import styles from "../../navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-export default function Pilotos() {
-  const [pilotos] = useState([
-    "Francesco Bagnaia",
-    "Marc Márquez",
-    "Fabio Quartararo",
-    "Jorge Martín",
-  ]);
-  const [busqueda, setBusqueda] = useState("");
+function Pilotos() {
+  const [pilotos, setPilotos] = useState([]);
+
+  async function fetchPilotos() {
+    const response = await fetch("/api/pilotos/");
+    const data = await response.json();
+    setPilotos(data);
+  }
+
+  useEffect(() => {
+    fetchPilotos();
+  }, []);
 
   return (
-    <div>
-      <div>
-        <nav className={`navbar navbar-expand-lg navbar-dark ${styles.navbar}`}>
-          <div className="container">
-            {/* Botón para colapsar la barra en pantallas pequeñas */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavCentered"
-              aria-controls="navbarNavCentered"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+    <div className="container-fluid">
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavCentered"
+            aria-controls="navbarNavCentered"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            <div className="collapse navbar-collapse" id="navbarNavCentered">
-              <div className="row w-100 align-items-center">
-                {/* Sección izquierda con algunos enlaces */}
-                <div className="col-4 d-flex justify-content-start">
-                  <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link href="/pages/calendario/" className="nav-link fs-5">
-                        Calendario
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/pages/contacto" className="nav-link fs-5">
-                        Contacto
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-
-                {/* Sección central con el logo y el texto */}
-                <div className="col-4 d-flex justify-content-center">
-                  <Link
-                    href="../"
-                    className="navbar-brand d-flex flex-column align-items-center"
-                  >
-                    <img
-                      src="/images/logoMotogpFacil.png"
-                      alt="MotoGP Facil"
-                      style={{ width: "50px", height: "auto" }}
-                    />
-                    <span className="fs-4">MotoGP Facil</span>
-                  </Link>
-                </div>
-
-                {/* Sección derecha con el resto de enlaces */}
-                <div className="col-4 d-flex justify-content-end">
-                  <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link href="/pages/equipos" className="nav-link fs-5">
-                        Equipos
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/pages/pilotos" className="nav-link fs-5">
-                        Pilotos
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/pages/reglamento" className="nav-link fs-5">
-                        Reglamento
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
+          <div className="collapse navbar-collapse" id="navbarNavCentered">
+            <div className="row w-100 align-items-center">
+              <div className="col-4 d-flex justify-content-start">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link href="/pages/calendario/" className="nav-link fs-5">
+                      Calendario
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/pages/contacto" className="nav-link fs-5">
+                      Contacto
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-4 d-flex justify-content-center">
+                <Link
+                  href="../"
+                  className="navbar-brand d-flex flex-column align-items-center"
+                >
+                  <img
+                    src="/images/logoMotogpFacil.png"
+                    alt="MotoGP Facil"
+                    style={{ width: "50px", height: "auto" }}
+                  />
+                  <span className="fs-4">MotoGP Facil</span>
+                </Link>
+              </div>
+              <div className="col-4 d-flex justify-content-end">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link href="/pages/equipos" className="nav-link fs-5">
+                      Equipos
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/pages/pilotos" className="nav-link fs-5">
+                      Pilotos
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/pages/reglamento" className="nav-link fs-5">
+                      Reglamento
+                    </Link>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-        </nav>
+        </div>
+      </nav>
+
+      {/* Título */}
+      <div className="py-4 text-center">
+        <h1>PILOTOS MOTOGP</h1>
       </div>
-      <br></br>
-      <h1>Pilotos de MotoGP</h1>
-      <input
-        type="text"
-        placeholder="Buscar piloto..."
-        value={busqueda}
-        onChange={(e) => setBusqueda(e.target.value)}
-      />
-      <ul>
-        {pilotos
-          .filter((piloto) =>
-            piloto.toLowerCase().includes(busqueda.toLowerCase())
-          )
-          .map((piloto, index) => (
-            <li key={index}>{piloto}</li>
+
+      {/* Grid de Pilotos */}
+      <div className="container">
+        <div className="row justify-content-center">
+          {pilotos.map((piloto) => (
+            <div key={piloto.id} className="col-12 col-md-6 col-lg-4 mb-4">
+              <div className="card">
+                <img
+                  src={`/images/pilotos/${piloto.id}.png`}
+                  className="card-img-top img-fluid"
+                  alt={piloto.nombre}
+                  style={{ maxHeight: "150px", objectFit: "contain" }}
+                />
+                <div className="card-body text-center">
+                  <h5 className="card-title">
+                    {piloto.nombre} {piloto.apellidos}
+                  </h5>
+                  <p className="card-text">
+                    <img
+                      src={`/images/numeros/${piloto.numero}.png`}
+                      alt={piloto.numero}
+                      style={{ width: "30px", marginRight: "5px" }}
+                    />
+                    <img
+                      src={`/images/banderas/${piloto.pais}.png`}
+                      alt={piloto.pais}
+                      style={{ width: "30px" }}
+                    />
+                  </p>
+                  <p className="card-text">
+                    Moto3: {piloto.titulos_moto3} | Moto2: {piloto.titulos_moto2} | MotoGP: {piloto.titulos_motogp}
+                  </p>
+                </div>
+              </div>
+            </div>
           ))}
-      </ul>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Pilotos;
